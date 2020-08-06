404461a98df9c28e94e4b1f12a7e5aabade80709
boubamg <bouba.maiga99@gmail.com>
2020-08-06 20:45:46 +0200

    changing 'index.js' structure

--- Frontend/js/index.js
+++ Frontend/js/index.js
@@ -1,20 +1,37 @@
-var request = new XMLHttpRequest();
-request.onreadystatechange = function() {
-    if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
+// Connection to API
+var get = function(url, success, error){
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function(){
+        if (this.readyState === 4 && this.status == 200){
+            success(this.responseText);
+        } else {
+            error(request);
+        }
+    }
+    request.open("GET", url);
+    request.send();
+}
 
-            // Create ul tag
+// Display Teddies
+var getPost = function(){
+
+    get("http://localhost:3000/api/teddies",
+
+    // Success Params
+    function(response){
+
+        var teddies = JSON.parse(response);
+        console.log(teddies)
+
+        var container = document.querySelector(".container");
+
             var ulElement = document.createElement("ul");
+            container.appendChild(ulElement)
 
-            // Put <ul> in container class
-            var container = document.querySelector(".container");
-            container.appendChild(ulElement);
+        // Loop for each teddie object
+        for(var i = 0; i < teddies.length; i++ ) {
             
-
-        var response = JSON.parse(this.responseText); 
-        for(var i = 0; i < response.length; i++ ) {
-
             // Creation of Tag
-
             var liElement = document.createElement("li");
             var a1Element = document.createElement("a");
             var a2Element = document.createElement("a");
@@ -26,28 +43,33 @@ request.onreadystatechange = function() {
             var price = document.createElement("span");
                 price.classList.add("price");
 
+            // Display informations for each Teddies
+            liElement.setAttribute("id", teddies[i]._id);
+            imgElement.setAttribute("src", teddies[i].imageUrl);
+            h3Element.textContent = teddies[i].name;
+            price.textContent = teddies[i].price + " €";
 
-            liElement.setAttribute("id", response[i]._id);
-            imgElement.setAttribute("src", response[i].imageUrl);
-            h3Element.textContent = response[i].name;
-            price.textContent = response[i].price + " €";
-
+            // Add links
             a1Element.setAttribute("href", "./pages/produit.html");
             a1Element.appendChild(imgElement);
             
             a2Element.setAttribute("href", "./pages/produit.html");
             a2Element.appendChild(h3Element);
 
+            // Built structure
             divElement.appendChild(a2Element);
             divElement.appendChild(price);
             divElement.appendChild(buttonAdd);
-
             liElement.appendChild(a1Element);
             liElement.appendChild(divElement);
             ulElement.appendChild(liElement);
         }
-    }
+    }, 
+    // Error Params
+    function(error){
+        console.log("Erreur AJAX : " + error)
+    })
 }
 
-request.open("GET", "http://localhost:3000/api/teddies");
-request.send();
+// Function call
+getPost();
