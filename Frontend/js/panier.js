d9ffb501d4848d649e186a45b30fda801c697373
boubamg <bouba.maiga99@gmail.com>
2020-08-11 17:39:28 +0200

    creation of basket display

--- Frontend/js/panier.js
+++ Frontend/js/panier.js
@@ -1,9 +1,10 @@
 // Articles in Basket
 const basket = localStorage.getItem('basket');
+const basketParsed = JSON.parse(basket);
 
 // Main Function 
 const basketManagement = function(){
-    if (basket){
+    if (basketParsed != 0){
         getBasketNb();
         getBasket();
 
@@ -19,12 +20,69 @@ const getBasket = () => {
 
     // Creation of elements
     var h1Element = document.createElement("h1");
+        h1Element.textContent = "Orinoco - Panier";
+    var ulElement = document.createElement("ul");
+        ulElement.classList.add("basketList");
 
-    // Elements Content
-    h1Element.textContent = "Orinoco - Panier";
-
-    // Structure
     container.appendChild(h1Element);
+    container.appendChild(ulElement);
+
+    for(var i = 0; i < basketParsed.length; i++){
+
+        var selectTeddie = basketParsed[i];
+
+        // Create Element
+        var liElement = document.createElement("li");
+
+        var divImgBasket = document.createElement("div");
+            divImgBasket.classList.add("imgBasket");
+        var divInfoBasket = document.createElement("div");
+            divInfoBasket.classList.add("infoBasket");
+        var divPriceBasket = document.createElement("div");
+            divPriceBasket.classList.add("priceBasket")
+
+        var imgTeddie = document.createElement("img");
+            imgTeddie.classList.add("img-fluid");
+        var nameTeddie = document.createElement("h3");
+        var priceTeddie = document.createElement("span");
+            priceTeddie.classList.add("price");
+        var deleteButton = document.createElement("a");
+            deleteButton.href = "javascript:window.location.reload()";
+
+
+        deleteButton.addEventListener("click", function(){
+
+            // Array of Teddies for basket
+            var teddieArray = localStorage.getItem("basket") ? JSON.parse(localStorage.getItem("basket")) : [];
+            
+            // Delete Teddie object in Array
+            teddieArray.splice( selectTeddie, 1 );
+
+            // Save teddie array in localstorage basket
+            localStorage.setItem("basket", JSON.stringify(teddieArray));
+
+            getBasketNb()
+        });
+        
+
+        // Element Content
+        imgTeddie.src = selectTeddie.image;
+        nameTeddie.textContent = selectTeddie.name;
+        priceTeddie.textContent = selectTeddie.price;
+        deleteButton.textContent = "Supprimer";
+
+        // Structure
+        divImgBasket.appendChild(imgTeddie);
+        divInfoBasket.appendChild(nameTeddie);
+        divInfoBasket.appendChild(deleteButton);
+        divPriceBasket.appendChild(priceTeddie);
+
+        liElement.appendChild(divImgBasket);
+        liElement.appendChild(divInfoBasket);
+        liElement.appendChild(divPriceBasket);
+
+        ulElement.appendChild(liElement);
+    }
     
 }
 
@@ -54,5 +112,7 @@ var getBasketNb = function(){
      NbItem = localStorage.getItem("basket") ? JSON.parse(localStorage.basket) : 0;
      basketNb.textContent = NbItem.length;
 }
+
+console.log(basketParsed);
 
 basketManagement();
