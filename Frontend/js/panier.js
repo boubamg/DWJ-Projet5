833f4717c7304cfd50c7c8c652dc07f9af9993df
boubamg <bouba.maiga99@gmail.com>
2020-08-11 11:50:49 +0200

    adding comments and h1element in 'getBasket' function

--- Frontend/js/panier.js
+++ Frontend/js/panier.js
@@ -1,5 +1,7 @@
+// Articles in Basket
 const basket = localStorage.getItem('basket');
 
+// Main Function 
 const basketManagement = function(){
     if (basket){
         getBasketNb();
@@ -10,8 +12,19 @@ const basketManagement = function(){
     }
 }
 
+// Get all articles in Basket
 const getBasket = () => {
 
+    var container = document.querySelector(".container");
+
+    // Creation of elements
+    var h1Element = document.createElement("h1");
+
+    // Elements Content
+    h1Element.textContent = "Orinoco - Panier";
+
+    // Structure
+    container.appendChild(h1Element);
     
 }
 
@@ -35,9 +48,8 @@ const emptyBasket = function(){
 
 }
 
-
-var getBasketNb = function(){
-     // See Nb Item in Basket
+ // See Nb Item in Basket
+var getBasketNb = function(){ 
      var basketNb = document.querySelector(".fa-shopping-bag");
      NbItem = localStorage.getItem("basket") ? JSON.parse(localStorage.basket) : 0;
      basketNb.textContent = NbItem.length;
