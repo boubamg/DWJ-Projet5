bb0e595c0774b67313f179ef7c41a29aaf32caec
boubamg <bouba.maiga99@gmail.com>
2020-08-11 11:24:48 +0200

    creation of 'emptyBasket' function

--- Frontend/js/panier.js
+++ Frontend/js/panier.js
@@ -1,8 +1,31 @@
+const basket = localStorage.getItem('basket');
 
 const basketManagement = function(){
-    if (localStorage.length != 0){
+    if (basket){
         getBasket();
     } else {
         emptyBasket();
     }
 }
+
+const emptyBasket = function(){
+
+    var container = document.querySelector(".container");
+
+    var h1Element = document.createElement("h1");
+    var emptyBasketText = document.createElement("p");
+    var homeButton = document.createElement("a");
+
+    h1Element.textContent = "Orinoco - Panier";
+    emptyBasketText.textContent = "Votre panier est vide, cliquez ci-dessous pour voir nos articles.";
+    homeButton.textContent = "Tous les articles";
+    homeButton.classList.add("btn");
+    homeButton.href = "../index.html";
+
+    container.appendChild(h1Element);
+    container.appendChild(emptyBasketText);
+    container.appendChild(homeButton);
+
+}
+
+basketManagement();
