72563e8da28158daca955ac0636f3ce7e4f613ab
boubamg <bouba.maiga99@gmail.com>
2020-08-17 15:47:50 +0200

    creation of summary section and 2 functions : 'displayCommand' and 'getTotalPrice'

--- Frontend/js/confirmation.js
+++ Frontend/js/confirmation.js
@@ -1,14 +1,47 @@
-var ordered = localStorage.getItem("order") ? JSON.parse(localStorage.getItem("order")) : [];
+// Variable
+const ordered = localStorage.getItem("order") ? JSON.parse(localStorage.getItem("order")) : [];
+const basketParsed = localStorage.getItem("basket") ? JSON.parse(localStorage.getItem("basket")) : [];
 
-// Create Element
-var thanksDiv = document.createElement("div");
-thanksDiv.classList.add("thankyou");
-var thanksP = document.createElement("p");
-thanksDiv.textContent = "Merci " + ordered.contact.firstName + " ! À bientot" 
-thanksDiv.appendChild(thanksP);
+var displayCommand = () => {
+    // Create Element * SECTION VALIDATE *
+    var thanksDiv = document.createElement("div");
+        thanksDiv.classList.add("thankyou");
+    var thanksP = document.createElement("p");
+        thanksP.textContent = "Merci " + ordered.contact.firstName + ". À bientot !" 
+    thanksDiv.appendChild(thanksP);
 
-// Html Element
-var container = document.querySelector(".container");
-var validateSection = document.querySelector(".validate");
+    // Create Element * SECTION SUMMARY *
+    var orderId = document.createElement("p");
+        orderId.textContent = ordered.orderId;
+    var totalPrice = document.createElement("p");
+        totalPrice.textContent = getTotalPrice();
 
-validateSection.appendChild(thanksDiv);
+    var orderIdDiv = document.createElement("div");
+        orderIdDiv.classList.add("orderId");
+    var totalPriceDiv = document.createElement("div");
+        totalPriceDiv.classList.add("totalPriceConfirmation");
+
+    orderIdDiv.appendChild(orderId);
+    totalPriceDiv.appendChild(totalPrice);
+
+    // Html Element
+    var container = document.querySelector(".container");
+    var validateSection = document.querySelector(".validate");
+    var summarySection = document.querySelector(".summary");
+
+    // Construction Structure
+    validateSection.appendChild(thanksDiv);
+
+    summarySection.appendChild(orderIdDiv);
+    summarySection.appendChild(totalPriceDiv);
+}
+
+var getTotalPrice = function(){
+    var price = 0;
+    for(var i = 0; i < basketParsed.length; i++){
+        price += basketParsed[i].price;
+    }
+    return price;
+}
+
+displayCommand();
